9dcca132a5534268cb3b763b014b8991791c4bd7
12W300 <78951373+12W300@users.noreply.github.com>
2021-12-13 11:48:35 +0300

    Add files via upload

---
+++ ind13.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    u = set("abcdefghijklmnopqrstuvwxyz")
+
+    a = {"a", "b", "g", "k", "m", "p"}
+    b = {"b", "e", "f", "l", "r"}
+    c = {"k", "l", "w", "x"}
+    d = {"e", "j", "o", "p", "q", "u", "v"}
+
+    x = (a.difference(b)).intersection(c.union(d))
+    print(f"x = {x}")
+
+    an = u.difference(a)
+    bn = u.difference(b)
+
+    y = (an.intersection(bn)).difference(c.union(d))
+    print(f"y = {y}")
